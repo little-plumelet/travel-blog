e8a19e5110e1a46b63ad67d115f606144ae39b3c
Anastasiia Perova <perovaa.24@gmail.com>
2024-12-02 16:14:03 +0400

    feat: add Header animation on scroll with framer-motion

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+"use client";
+
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import React, { useState } from "react";
+
 import CountrySelector from "../CountrySelector";
 import LanguageSelector from "../LanguageSelector";
 import Logo from "../Logo";
@@ -7,8 +11,29 @@ import AnimatedBurger from "./parts/AnimatedBurger";
 import s from "./Header.module.scss";
 
 function Header() {
+  const { scrollY } = useScroll();
+  const [isVisible, setIsVisible] = useState(true);
+
+  useMotionValueEvent(scrollY, "change", (current) => {
+    const previous = scrollY.getPrevious() ?? 0;
+    const diff = current - previous;
+
+    // If scrolling down, hide the header; if up, show it
+    if (diff > 0 && isVisible) {
+      setIsVisible(false);
+    } else if (diff < 0 && !isVisible) {
+      setIsVisible(true);
+    }
+  });
+
+  console.log(isVisible);
   return (
-    <header className={s.wrapper}>
+    <motion.header
+      className={s.wrapper}
+      initial={{ y: 0 }} // Start visible
+      animate={{ y: isVisible ? 0 : "-100%" }} // Move out of view when hidden
+      transition={{ type: "tween", duration: 0.3 }} // Smooth transition
+    >
       <Logo />
       <div className={s.item}>
         <CountrySelector />
@@ -17,7 +42,7 @@ function Header() {
         <LanguageSelector />
       </div>
       <AnimatedBurger />
-    </header>
+    </motion.header>
   );
 }
 
